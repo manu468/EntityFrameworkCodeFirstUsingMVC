05ff79f07baece0d9ced948e2f8ae61db58053ba
manu468 <manojmuppavarapu@gmail.com>
2020-02-15 11:27:47 -0500

    Add project files.

---
+++ MuppavarapuStateUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using MuppavarapuStateUniversity.Models;
+
+namespace MuppavarapuStateUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //The modelBuilder.Conventions.Remove statement in the OnModelCreating method prevents table
+            //    names from being pluralized.If you didn't do this, the generated tables in the database
+            //    would be named Students, Courses, and Enrollments. Instead, the table names will be Student, 
+            //    Course, and Enrollment
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
+//This code creates a DbSet property for each entity set.In Entity Framework terminology, 
+//    an entity set typically corresponds to a database table, and an entity corresponds to a row in the table.
---
+++ MuppavarapuStateUniversity/Models/Enrollment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MuppavarapuStateUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        //The CourseID property is a foreign key, and the corresponding navigation property is Course.
+        //    An Enrollment entity is associated with one Course entity.
+        public int CourseID { get; set; }
+        //The StudentID property is a foreign key, and the corresponding navigation property is Student.
+        //    An Enrollment entity is associated with one Student entity, so the property can only hold a 
+        //    single Student entity(unlike the Student.Enrollments navigation property we saw earlier,
+        //        which can hold multiple Enrollment entities).
+        public int StudentID { get; set; }
+        //The Grade property is an enum. The question mark after the Grade type declaration 
+        //    indicates that the Grade property is nullable.A grade that's null is different 
+        //    from a zero grade — null means a grade isn't known or hasn't been assigned yet.
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
